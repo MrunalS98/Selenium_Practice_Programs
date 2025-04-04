73a4589ff85afe392abd14b70d5ef3c814ea8c8d
MrunalSolanki <mrunalquora1234@gmail.com>
2025-04-04 18:52:23 +0530

    Part1_Selenium

--- src/test/java/com/MrunalSolanki/Oepn_Browser/FireFoxBrowser.java
+++ src/test/java/com/MrunalSolanki/Oepn_Browser/FireFoxBrowser.java
@@ -1,2 +1,16 @@
-package com.MrunalSolanki.Oepn_Browser;public class FireFoxBrowser {
-}
+package com.MrunalSolanki.Oepn_Browser;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FireFoxBrowser {
+
+        @Test
+        @Description("Verify Browse is getting open with given link")
+        public void test_verify_browser(){
+            FirefoxDriver driver = new FirefoxDriver();
+            driver.get("https://app.vwo.com");
+            System.out.println(driver.getTitle());
+        }
+    }
